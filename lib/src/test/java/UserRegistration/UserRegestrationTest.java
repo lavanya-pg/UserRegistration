b12d9681357a2dea5f5c6d44e7d88091663e8266
lavanya-pg <lavanyapg8@gmail.com>
2021-07-13 21:37:25 +0530

    ADDED: Code For Enter A Valid LastName

--- lib/src/test/java/UserRegistration/UserRegestrationTest.java
+++ lib/src/test/java/UserRegistration/UserRegestrationTest.java
@@ -4,33 +4,57 @@ import org.junit.Assert;
 import org.junit.Test;
 public class UserRegestrationTest
 {
-
 	UserRegistration user = new UserRegistration();
 
-     @Test
-     public void FirstName_ShouldReturnTrue_LetterShouldBeUpperCase() 
-     {
+    @Test
+     public void FirstName_ShouldReturnTrue_LetterShouldbeUpperCase() 
+    {
     	boolean result = user.validateFirstName("Lavanya");
     	Assert.assertTrue(result);
-	 }
-     @Test
-     public void FirstName_ShouldReturnFalse_LetterShouldLessThanThreeChar()
-     {
-    	 boolean result = user.validateFirstName("La");
-    	 Assert.assertFalse(result);
-     }
-
-     @Test
- 	 public void FirstName_ShouldReturnFalse_ItHasNumber() 
-     {
+	}
+    @Test
+    public void FirstName_ShouldReturnFalse_LetterShouldLessthanThreeChar()
+    {
+    	boolean result = user.validateFirstName("La");
+	 	Assert.assertFalse(result);
+    }
+    @Test
+ 	public void FirstName_ShouldReturnFalse_ItHasNumber()
+    {
  		boolean result = user.validateFirstName("Lavi8");
  		Assert.assertFalse(result);
- 	 }
+ 	}
+    @Test
+    public void FirstName_ShouldReturnFalse_ItHasSpecialCharacter()
+    {
+    	boolean result = user.validateFirstName("Lavi!");
+    	Assert.assertFalse(result);
+    }
+    @Test
+	public void LastName_ShouldReturnTrue_LetterShouldbeUpperCase()
+    {
+	    boolean result = user.validateLastName("Gangadharan");
+	    Assert.assertTrue(result);
+	}
+	
+	@Test 
+	public void LastName_ShouldReturnFalse_LetterShouldLessthanThreeChar()
+	{
+		boolean result = user.validateLastName("Ga");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void LastName_ShouldReturnFalse_ItHasNumber()
+	{
+	      boolean result = user.validateFirstName("Gangadharan8");
+	      Assert.assertFalse(result);
+	}
 
-     @Test
-     public void FirstName_ShouldReturnFalse_ItHasSpecialCharacter() 
-     {
-    	 boolean result = user.validateFirstName("Lavi!");
-    	 Assert.assertFalse(result);
-     }
+	@Test
+	public void LastName_ShouldReturnFalse_ItHasSpecialCharacter()
+	{
+	      boolean result = user.validateFirstName("Ganga^dharan");
+	      Assert.assertFalse(result);
+	 }
 }
