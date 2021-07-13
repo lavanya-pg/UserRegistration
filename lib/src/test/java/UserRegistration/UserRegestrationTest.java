e96f7609711a314d16a126c4636df1aff425e916
lavanya-pg <lavanyapg8@gmail.com>
2021-07-13 22:59:35 +0530

    ADDED: Code For Enter A Validate MobileNumber

--- lib/src/test/java/UserRegistration/UserRegestrationTest.java
+++ lib/src/test/java/UserRegistration/UserRegestrationTest.java
@@ -4,44 +4,45 @@ import org.junit.Assert;
 import org.junit.Test;
 public class UserRegestrationTest
 {
+
 	UserRegistration user = new UserRegistration();
 
-     @Test
-     public void FirstName_ShouldReturnTrue_LetterShouldbeUpperCase() 
-     {
+    @Test
+    public void FirstName_ShouldReturnTrue_LetterShouldbeUpperCase() 
+    {
     	boolean result = user.validateFirstName("Lavanya");
 	 	Assert.assertTrue(result);
-	 }
-     @Test
-     public void FirstName_ShouldReturnFalse_LetterShouldLessthanThreeChar()
-     {
+	}
+    @Test
+    public void FirstName_ShouldReturnFalse_LetterShouldLessthanThreeChar()
+    {
     	 boolean result = user.validateFirstName("La");
-    	 Assert.assertFalse(result);
-     }
-     @Test
- 	 public void FirstName_ShouldReturnFalse_ItHasNumber() 
-     {
+	 	Assert.assertFalse(result);
+    }
+    @Test
+ 	public void FirstName_ShouldReturnFalse_ItHasNumber()
+    {
  		boolean result = user.validateFirstName("Lavi8");
  		Assert.assertFalse(result);
- 	 }
-     @Test
-     public void FirstName_ShouldReturnFalse_ItHasSpecialCharacter() 
-     {
+ 	}
+    @Test
+    public void FirstName_ShouldReturnFalse_ItHasSpecialCharacter()
+    {
     	boolean result = user.validateFirstName("Lavi!");
-    	Assert.assertFalse(result);
-      }
-      @Test
-	  public void LastName_ShouldReturnTrue_LetterShouldbeUpperCase() 
-	  { 
+		Assert.assertFalse(result);
+     }
+     @Test
+	 public void LastName_ShouldReturnTrue_LetterShouldbeUpperCase() 
+     {
 	    boolean result = user.validateLastName("Gangadharan");
 	    Assert.assertTrue(result);
 	  }
 	  @Test 
 	  public void LastName_ShouldReturnFalse_LetterShouldLessthanThreeChar()
 	  {
-		boolean result = user.validateLastName("Ga");
-		Assert.assertFalse(result);
-	  }
+		 boolean result = user.validateLastName("Ga");
+		 Assert.assertFalse(result);
+   	  }
 	  @Test
 	  public void LastName_ShouldReturnFalse_ItHasNumber() 
 	  {
@@ -49,15 +50,41 @@ public class UserRegestrationTest
 	      Assert.assertFalse(result);
 	  }
 	  @Test
-	  public void LastName_ShouldReturnFalse_ItHasSpecialCharacter()
-	  {
+	   public void LastName_ShouldReturnFalse_ItHasSpecialCharacter() 
+	   {
 	      boolean result = user.validateFirstName("Ganga^dharan");
 	      Assert.assertFalse(result);
-	  }
-	  @Test
-	  public void EmailId_ShouldReturnTrue_ItShouldBeProperFormat()
-	  {
+	   }
+	   @Test
+	   public void EmailId_ShouldReturnTrue_ItShouldBeProperFormat()
+	   {
 	        boolean result = user.validateEmailid("xyz@gdkd.com.in");
 	        Assert.assertTrue(result);
-	  }
+	   }
+	   @Test
+	   public void MobileNumber_ShouldReturnTrue_IfItHasSpacebetweenTheNumbers()
+	   {
+	        boolean result = user.validateMobileNum("91 9993588735");
+	        Assert.assertTrue(result);
+	    }
+	    @Test
+	    public void MobileNumber_ShouldReturnFalse_IfItHasNotSpacebetweenTheNumbers()
+	    {
+	        boolean result = user.validateMobileNum("919993588735");
+	        Assert.assertFalse(result);
+	    }
+
+	    @Test
+	    public void MobileNumber_ShouldReturnFalse_IfItHasLessdigits()
+	    {
+	        boolean result = user.validateMobileNum("966642173");
+	        Assert.assertFalse(result);
+	    }
+	    @Test
+	    public void MobileNumber_ShouldReturnFalse_IfItHasNotStartedWith91()
+	    {
+	        boolean result = user.validateMobileNum("7560984632645");
+	        Assert.assertFalse(result);
+	    }
+
 }
--- lib/src/test/java/UserRegistration/UserRegistration.java
+++ lib/src/test/java/UserRegistration/UserRegistration.java
@@ -5,11 +5,11 @@ import java.util.regex.Pattern;
 
 public class UserRegistration
 {
-
 	private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
 	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
-    
-    public boolean validateFirstName(String fname) 
+	private static final String MOBILE_PATTERN = "^[0-9]{0,2}[ ][0-9]{10}";
+	
+	public boolean validateFirstName(String fname)
     {
         return patternChecker(fname, NAME_PATTERN);
     }
@@ -21,6 +21,11 @@ public class UserRegistration
     {
         return patternChecker(Emailid, EMAIL_PATTERN);
     }
+    public boolean validateMobileNum(String mobileNum)
+    {
+		return patternChecker(mobileNum, MOBILE_PATTERN);
+	
+	}
     
     private boolean patternChecker(String input,String fieldPattern)
     {
